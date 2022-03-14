50273bec5dda2ced4e139cfebd4edaa2f77ff703
Kai Lacerna <iruelelacerna787>
2022-03-14 16:19:16 -0400

    + temperature.py and updated line 14 on temperature.html

---
+++ temperature.py
@@ -0,0 +1,3 @@
+celsius = input("Please enter a temperature in celcius ")
+fahrenheit = ((float(celsius) * 9/5) + 32)
+print(celsius + " degrees celsius is " + str(fahrenheit) + " degrees fahrenheit.")
