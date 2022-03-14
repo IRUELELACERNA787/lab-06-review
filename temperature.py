3537a7dbe85ce329099422372ea99de01a0ece3c
Kai Lacerna <iruelelacerna787>
2022-03-14 16:39:32 -0400

    updated temperature.py and temperature.html with rounded values and updated print statements

--- temperature.py
+++ temperature.py
@@ -1,3 +1,5 @@
 celsius = input("Please enter a temperature in celcius ")
 fahrenheit = ((float(celsius) * 9/5) + 32)
-print(celsius + " degrees celsius is " + str(fahrenheit) + " degrees fahrenheit.")
+fahrenheit = round(fahrenheit)
+
+print(celsius + " °C is " + str(fahrenheit) + " °F.")
